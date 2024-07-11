d67d64c094b4be4013c982ad82712df124b84572
Zryru <jorge.ale91@gmail.com>
2024-07-12 01:03:39 +0200

    test open ics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,15 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FileOpener } from '@awesome-cordova-plugins/file-opener/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    FileOpener,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Filesystem, Directory, Encoding } from '@capacitor/filesystem';
+import { FileOpener } from '@awesome-cordova-plugins/file-opener/ngx';
 
 @Component({
   selector: 'app-tabs',
@@ -7,6 +9,29 @@ import { Component } from '@angular/core';
 })
 export class TabsPage {
 
-  constructor() {}
+  constructor(private fileOpener: FileOpener) {}
+
+  async saveAndOpenICSFile() {
+    const icsData = 'BEGIN:VCALENDAR\nVERSION:2.0\nPRODID:-//hacksw/handcal//NONSGML v1.0//EN\nBEGIN:VEVENT\nUID:uid1@example.com\nDTSTAMP:20230711T120000Z\nORGANIZER;CN=John Doe:MAILTO:john.doe@example.com\nDTSTART:20230711T120000Z\nDTEND:20230711T130000Z\nSUMMARY:Test Event\nEND:VEVENT\nEND:VCALENDAR';
+    const fileName = 'event.ics';
+
+    try {
+      const result = await Filesystem.writeFile({
+        path: fileName,
+        data: icsData,
+        directory: Directory.Documents,
+        encoding: Encoding.UTF8
+      });
+      console.log('File written', result);
+
+      const filePath = result.uri;
+
+      this.fileOpener.open(filePath, 'text/calendar')
+        .then(() => console.log('File is opened'))
+        .catch(e => console.error('Error opening file', e));
+    } catch (e) {
+      console.error('Unable to write file', e);
+    }
+  }
 
 }
